84570f54613c7c2de1d4137bdfa183dcc014cff8
Valentine Bontemps <valveryne@hotmail.com>
2023-06-14 17:18:29 +0200

    Section skills en cours

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -1,10 +1,12 @@
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 // import { faStar } from '@fortawesome/free-regular-svg-icons'
 import skills from "../datas/skills.json"
+import styles from "./components.module.scss"
+
 export default function Skills() {
 
     const listSkills = skills.map((skill) => 
-    <li key={skill.id}>
+    <li key={skill.id} className={styles.oneSkill}>
         <h3>{skill.title}</h3>
         <p>{skill.rate}/5</p>
     </li>)
@@ -12,6 +14,6 @@ export default function Skills() {
     return ( 
     <article>
         <h2>Mes différentes compétences dans le domaine du développement web</h2>
-            <ul>{listSkills}</ul>
+            <ul className={styles.listSkills}>{listSkills}</ul>
     </article>)
 } 
