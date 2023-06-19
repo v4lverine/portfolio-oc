4947b475cf8862520644be64821da1ea4882d6f8
Valentine Bontemps <valveryne@hotmail.com>
2023-06-19 16:49:59 +0200

    Page contact en cours de création

--- app/components/ContactForm.js
+++ app/components/ContactForm.js
@@ -2,25 +2,53 @@
 "use client";
 import { useState } from "react";
 import styled from "styled-components";
+import styles from "./components.module.scss";
 
 const Button = styled.button`
   background: transparent;
   border-radius: 3px;
   border: 2px solid #bf4f74;
-  font-size: 1.5em;
+  font-size: 1em;
+  //   :active {
+  //     background-color: #000;
+  //   }
 `;
 
 export default function ContactForm() {
   const [formStatus, setFormStatus] = useState("Envoyer");
   return (
-    <section>
-      <form>
-        <label htmlFor="name">Prénom/Nom</label>
-        <input type="text" name="name" required />
-        <label>E-mail</label>
-        <input type="text" name="email" required />
-        <label>Message</label>
-        <textarea name="message" required></textarea>
+    <section className={styles.sectionForm}>
+      <form className={styles.contactForm} action="#">
+        <label htmlFor="name" className={styles.placeholder}>
+          Prénom/Nom
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Prénom / Nom"
+          required
+        />
+        <label htmlFor="email" className={styles.placeholder}>
+          E-mail
+        </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Adresse e-mail"
+          required
+        />
+        <label htmlFor="message" className={styles.placeholder}>
+          Message
+        </label>
+        <textarea
+          name="message"
+          minLength={100}
+          maxLength={2000}
+          placeholder="Message"
+          required
+        ></textarea>
         <Button>{formStatus}</Button>
       </form>
     </section>
