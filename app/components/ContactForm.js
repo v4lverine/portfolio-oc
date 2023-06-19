87cf1f47152a70e6c6f66638593c4ac0b5d9e119
Valentine Bontemps <valveryne@hotmail.com>
2023-06-19 11:09:44 +0200

    Avancée construction SASS du Footer

--- app/components/ContactForm.js
+++ app/components/ContactForm.js
@@ -1,27 +1,28 @@
 //en cours de fabrication
-"use client"; //comment ça marche ?
-import { useState } from "react"
-import styled from 'styled-components'
+"use client";
+import { useState } from "react";
+import styled from "styled-components";
 
 const Button = styled.button`
-background: transparent;
-border-radius: 3px;
-border: 2px solid #BF4F74;
-font-size: 1.5em`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #bf4f74;
+  font-size: 1.5em;
+`;
 
-export default function ContactForm(){
-    const [formStatus, setFormStatus] = useState('Envoyer')
-    return (
-        <section>
-            <form>
-                <label htmlFor="name">Prénom/Nom</label>
-                <input type="text" name="name" required/>
-                <label>E-mail</label>
-                <input type="text" name="email" required/>
-                <label>Message</label>
-                <textarea name="message" required></textarea>
-                <Button>{formStatus}</Button>
-            </form>
-        </section>
-    )
+export default function ContactForm() {
+  const [formStatus, setFormStatus] = useState("Envoyer");
+  return (
+    <section>
+      <form>
+        <label htmlFor="name">Prénom/Nom</label>
+        <input type="text" name="name" required />
+        <label>E-mail</label>
+        <input type="text" name="email" required />
+        <label>Message</label>
+        <textarea name="message" required></textarea>
+        <Button>{formStatus}</Button>
+      </form>
+    </section>
+  );
 }
