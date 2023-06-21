85171846ff0ac69ede4323ca28588510f9fbbbb5
Valentine Bontemps <valveryne@hotmail.com>
2023-06-21 15:43:02 +0200

    Header et footer retravaillés

--- app/components/ShortImage.js
+++ app/components/ShortImage.js
@@ -1,13 +1,7 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 const shortPicture = (props) => (
-    <Image
-    src={props.src}
-    height={100}
-    width={100}
-    quality={50}
-    alt={props.alt}
-    />
+  <Image src={props.src} height={80} width={80} quality={50} alt={props.alt} />
 );
 
-export default shortPicture
+export default shortPicture;
