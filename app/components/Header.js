e3ab457c25fd4cdb4b26c1f3455918029e8e9a64
Valentine Bontemps <valveryne@hotmail.com>
2023-06-21 12:08:33 +0200

    Scroll auto sous le header pour les liens d'ancrage + logo site cliquable

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,29 +1,31 @@
-"use client" //pour chargement côté client
-import styles from './components.module.scss' 
-import styled from 'styled-components'
-import Image from './ShortImage'
-import NavigateHeader from './NavigateHeader'
+"use client"; //pour chargement côté client
+import styles from "./components.module.scss";
+import styled from "styled-components";
+import Image from "./ShortImage";
+import NavigateHeader from "./NavigateHeader";
+import Link from "next/link";
 // import React, {useState, useEffect} from 'react'
 
 const BlockTitle = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    width: 100%;
-    margin-left: 50px
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  margin-left: 50px;
+`;
 
-export default function Header(){
-
-    return(
-        <header className={styles.header}>
-            <BlockTitle>
-                <Image src='/images/icon.jpg' alt='Logo du site de valverine'/>
-                <h1 className={styles.h1}>valverine - Développeuse web</h1>
-            </BlockTitle>
-            <nav>
-                <NavigateHeader />
-            </nav>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <BlockTitle>
+        <Link href="/">
+          <Image src="/images/icon.jpg" alt="Logo du site de valverine" />
+        </Link>
+        <h1 className={styles.h1}>valverine - Développeuse web</h1>
+      </BlockTitle>
+      <nav>
+        <NavigateHeader />
+      </nav>
+    </header>
+  );
 }
