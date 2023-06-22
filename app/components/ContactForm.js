15d72a2d50f4c8fc28b12f899daae94be8e47178
Valentine Bontemps <valveryne@hotmail.com>
2023-06-22 17:37:44 +0200

    Liaison méthode POST pour formulaire contact

--- app/components/ContactForm.js
+++ app/components/ContactForm.js
@@ -21,7 +21,37 @@ const raleway = Raleway({
 });
 
 export default function ContactForm() {
-  const [formStatus, setFormStatus] = useState("Envoyer");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submit, setSubmit] = useState("false");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //prevent page refreshing
+    console.log("Envoyer");
+    let data = {
+      name,
+      email,
+      message,
+    };
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("Réponse reçue ! 👻");
+      if (res.status === 200) {
+        alert("Votre message a bien été envoyé ! Merci ! 👍");
+        console.log("Réponse réussie ! ✅");
+        setSubmit("true");
+        setMessage("");
+        setEmail("");
+        setName("");
+      }
+    });
+  };
   return (
     <section className={styles.sectionForm}>
       <form className={styles.contactForm} action="#">
@@ -32,31 +62,50 @@ export default function ContactForm() {
           type="text"
           name="name"
           id="name"
+          value={name}
           placeholder="Prénom / Nom"
           required
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
         />
         <label htmlFor="email" style={{ visibility: "hidden" }}>
           E-mail
         </label>
         <input
-          type="text"
+          type="email"
           name="email"
           id="email"
+          value={email}
           placeholder="Adresse e-mail"
           required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <label htmlFor="message" style={{ visibility: "hidden" }}>
           Message
         </label>
         <textarea
           name="message"
-          minLength={100}
+          // minLength={100}
           maxLength={2000}
           id="message"
-          placeholder="Message"
+          value={message}
+          placeholder="Message (100 caractères minimum)"
           required
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }} //for changing inside form to capture input changes
         ></textarea>
-        <Button className={raleway.className}>{formStatus}</Button>
+        <Button
+          className={raleway.className}
+          onClick={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          Envoyer
+        </Button>
       </form>
     </section>
   );
