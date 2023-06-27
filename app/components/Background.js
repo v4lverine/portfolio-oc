28ac9b93c7e573c6d042fe87c36b62d37037351d
Valentine Bontemps <valveryne@hotmail.com>
2023-06-27 16:06:29 +0200

    Responsive mobile en cours de développement (va évoluer)

--- app/components/Background.js
+++ app/components/Background.js
@@ -3,14 +3,15 @@ import Image from "next/image";
 const backgroundImage = (props) => (
   <Image
     src={props.src}
-    fill
-    quality={80}
-    sizes="(max-width: 768px) 50vw, 33vw, (max-width: 1200px), 100 vw "
+    height={1500}
+    width={1600}
+    quality={50}
+    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
     alt={props.alt}
     // placeholder='blur'
     // blurDataURL='' lorsque l'image aura un domaine (?), ajouter via next.config.js
     priority
-    style={{ zIndex: "-2" }}
+    style={{ zIndex: "-2", position: "fixed" }}
   />
 );
 
