cbbada4a79d6f756ac9a0a922b9666b35cfffa2a
Valentine Bontemps <valveryne@hotmail.com>
2023-07-03 17:08:50 +0200

    Création paragraphe pour bloc Skills

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -13,6 +13,22 @@ export default function Skills() {
   return (
     <article className={styles.skillsBlock}>
       <h2 id="skills">Mes différentes compétences</h2>
+      <p className={styles.skillsText}>
+        Au cours de ma formation <b className={styles.keyword}>Web Developer</b>{" "}
+        au sein d&apos;
+        <a
+          href="https://openclassrooms.com"
+          target="_blank"
+          className={styles.linkSkillsText}
+        >
+          OpenClassrooms
+        </a>
+        , j'ai pu développer un grand nombre de compétences dans le domaine du
+        développement web. <br />
+        D'une note allant de 1 &#40;en cours d'apprentissage&#41; à 5
+        &#40;compétence bien acquise&#41;, j'ai auto-évalué ces divers atouts
+        dans cette liste ci-dessous.
+      </p>
       <ul className={styles.listSkills}>{listSkills}</ul>
     </article>
   );
