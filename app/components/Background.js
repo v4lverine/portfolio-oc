2c23bde09b9f522497b237f9f90bcd5bd2a7d0c5
Valentine Bontemps <valveryne@hotmail.com>
2023-07-04 15:07:52 +0200

    Recadrage background + hauteur bloc form

--- app/components/Background.js
+++ app/components/Background.js
@@ -4,9 +4,8 @@ const backgroundImage = (props) => (
   <Image
     src={props.src}
     height={1500}
-    width={1600}
+    width={2560}
     quality={50}
-    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
     alt={props.alt}
     // placeholder='blur'
     // blurDataURL='' lorsque l'image aura un domaine (?), ajouter via next.config.js
