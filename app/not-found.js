f88fc0ba0deb1fe9469026babd7711e3a34f9407
Valentine Bontemps <valveryne@hotmail.com>
2023-07-17 16:06:06 +0200

    Correctifs responsive v2

--- app/not-found.js
+++ app/not-found.js
@@ -4,19 +4,25 @@ import Link from "next/link";
 
 const ErrorPage = styled.section`
   background: var(--beigish);
-  margin: 5% 20%;
+  margin: 15% 5%;
   padding: 5%;
   border-radius: 25px;
   height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   h2 {
     margin: 0;
     text-align: center;
     font-size: 4em;
   }
   @media (max-width: 767px) {
-    font-size: 0.7em;
-    margin: 10% 2%;
+    font-size: 0.6em;
+    margin: 5% 3%;
     height: 400px;
+    h2 {
+      font-size: 3.2em;
+    }
   }
   @media (max-width: 1023px) {
     font-size: 0.8em;
@@ -28,7 +34,6 @@ export default function NotFound() {
     <main>
       <ErrorPage>
         <h2>Une erreur est survenue !</h2>
-        <hr style={{ marginBlock: "35px" }} />
         <Link
           href="/"
           style={{
@@ -38,6 +43,9 @@ export default function NotFound() {
             display: "block",
             margin: "auto",
             width: "fit-content",
+            textAlign: "center",
+            border: "2px solid var(--greyish)",
+            padding: "20px",
           }}
         >
           Retourner à la page d&apos;accueil
